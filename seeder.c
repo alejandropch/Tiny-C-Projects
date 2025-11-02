fc878fb0e40d622ae0ee364664ee299b47d1ae42
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-02 18:58:36 -0500

    seeder.c: better way for seeding data

--- seeder.c
+++ seeder.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <sqlite3.h>
+#include "service.h"
 
 typedef struct {
   char *text;
@@ -19,27 +20,28 @@ void seeder(sqlite3 *db, char * your_name, char *err){
   int namelen = strlen(your_name);
   clock = localtime(&now);
   Messages messages[] = {
-    ", my boy... why are you still awake? Hard programming night I see... I sense it..\n",
-    "!, Use your time wisely, you will. Trust in your judgement, I do\n",
-    ", the chosen one you are\n",
-    ", remember this you will. The dark side of the force, only the weak embrace it\n"
+    {", my boy... why are you still awake? Hard programming night I see... I sense it..\n", "em"},
+    {"!, Use your time wisely, you will. Trust in your judgement, I do\n", "m"},
+    {", the chosen one you are\n", "a"},
+    {", remember this you will. The dark side of the force, only the weak embrace it\n", "e"}
   };
-  buffer = (char *) malloc(namelen + strlen(messages[0]) + 1);
-  for (int i = 1; i < sizeof(messages) / sizeof(messages[0]); i++){
+  buffer = (char *) malloc(namelen + sizeof(messages[0]) * 2 + 1);
+  if(buffer == NULL){
+    fprintf(stderr, "Unable to allocate memory to buffer");
+    return;
+  }
+  for (int i = 0; i < sizeof(messages) / sizeof(messages[0]); i++){
+    if(sizeof(messages[i]) + namelen + 1 > sizeof(buffer))
+    {
+      fprintf(stdout, "Reallocating memory to buffer");
+      buffer = realloc(buffer, (sizeof(messages[i]) + namelen + 1) * 2);
+      if(buffer == NULL){
+        fprintf(stderr, "Unable to allocate memory to buffer");
+        return;
+      }
+    }
     sprintf(buffer, "%s%s", your_name, str);
     insert_wise_message(buffer, "em", db, err);
   }
-  str = ;
-  buffer = realloc(buffer, namelen + sizeof(str) + 1);
-  sprintf(buffer, "%s%s", your_name, str);
-  insert_wise_message(buffer, "m", db, err);
-  str = ;
-  buffer = realloc(buffer, namelen + sizeof(str) + 1);
-  sprintf(buffer, "%s%s", your_name, str);
-  insert_wise_message(buffer, "a", db, err);
-  str = ;
-  buffer = realloc(buffer, namelen + sizeof(str) + 1);
-  sprintf(buffer, "%s%s", your_name, str);
-  insert_wise_message(buffer, "e", db, err);
   free(buffer);
 }
