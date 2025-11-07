37269bfee6401b8b049ac01805b275d5ee0b365b
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-06 22:04:40 -0500

    service: using sqlite API

--- service.c
+++ service.c
@@ -1,9 +1,18 @@
+#include <stdio.h> 
 #include <sqlite3.h>
 
-void insert_wise_message(char *s, char *day_time, sqlite3 *db, char *err){
+void insert_wise_message(char *s, char *day_time, sqlite3 *db, char *err)
+{
   printf("message: %s\n", s);
-  const char sql_create[] = "INSERT INTO Messages (text) VALUES %s";
-  char buffer[strlen(sql_create) + 255];
-  sprintf(buffer, sql_create, s);
-  sqlite3_exec(db, sql_create, 0, 0, &err);
+  sqlite3_stmt *smt;
+  const char sql_create[] = "INSERT INTO Messages (text) VALUES ('?')";
+  sqlite3_prepare_v2(db, sql_create, -1, &smt, 0);  
+  sqlite3_bind_text(smt, 1, s, -1, SQLITE_STATIC);
+  int rc = sqlite3_step(smt);
+  if(rc == SQLITE_DONE){
+    sqlite3_finalize(smt);
+  }else{
+    fprintf(stderr, "insert wise message failed bro");
+    return;
+  }
 }
