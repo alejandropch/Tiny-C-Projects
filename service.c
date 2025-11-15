2c52b3576a388c4933a70f7458d9477bdeab0219
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-14 23:19:53 -0500

    service: typos and syntax errors fixed

--- service.c
+++ service.c
@@ -26,7 +26,7 @@ void print_random_message(sqlite3 *db, char *err)
 {
   int rc;
   time_t now;
-  sqlite3_stmt *stmr;
+  sqlite3_stmt *stm;
   struct tm *clock;
   time(&now);
   char *buffer;
@@ -34,30 +34,30 @@ void print_random_message(sqlite3 *db, char *err)
   const char sql_select[] = "SELECT message, daytime FROM messages WHERE (:hash_id)";
   const char sql_count[] = "SELECT COUNT(*) FROM messages";
 
-  rc = sqlite3_prepare_v2(db, sql_count, -1, &smt, NULL);
-  if(exploded(&rc, db, err));
-
-  rc = sqlite3_prepare_v2(db, sql_select, -1, &smt, NULL);
+  rc = sqlite3_prepare_v2(db, sql_count, -1, &stm, NULL);
+  if(exploded(&rc, db, err)) return;
+  rc = sqlite3_prepare_v2(db, sql_select, -1, &stm, NULL);
+  if(exploded(&rc, db, err)) return;
 
 }
 void insert_wise_message(char *s, char *day_time, sqlite3 *db, char *err)
 {
   int m, dt, rc;
-  sqlite3_stmt *smt;
+  sqlite3_stmt *stm;
   const char sql_create[] = "INSERT INTO messages (text, daytime) VALUES (:message, :daytime)";
-  rc = sqlite3_prepare_v2(db, sql_create, -1, &smt, 0);
+  rc = sqlite3_prepare_v2(db, sql_create, -1, &stm, 0);
   if(exploded(&rc, db, err)) return;
   // getting the index of my paramters (This is unnecessary but i want to learn more about the SQLite3 interface)
-  m = sqlite3_bind_parameter_index(smt, ":message");
-  dt = sqlite3_bind_parameter_index(smt, ":daytime");
+  m = sqlite3_bind_parameter_index(stm, ":message");
+  dt = sqlite3_bind_parameter_index(stm, ":daytime");
 
-  rc = sqlite3_bind_text(smt, m, s, -1, SQLITE_STATIC);
+  rc = sqlite3_bind_text(stm, m, s, -1, SQLITE_STATIC);
   if(exploded(&rc, db, err)) return;
-  rc = sqlite3_bind_text(smt, dt, s, -1, SQLITE_STATIC);
+  rc = sqlite3_bind_text(stm, dt, s, -1, SQLITE_STATIC);
   if(exploded(&rc, db, err)) return;
-  rc = sqlite3_step(smt);
+  rc = sqlite3_step(stm);
   if(rc == SQLITE_DONE){
-    sqlite3_finalize(smt);
+    sqlite3_finalize(stm);
     printf("message: %s\n", s);
   }else{
     fprintf(stderr, "insert wise message failed bro");
