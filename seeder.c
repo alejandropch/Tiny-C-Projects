a82291e8d558543cdc31eea78aca3ffa9944be2a
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-16 16:19:37 -0500

    seeder.c: using enums's keys for managing the daytimes, instead of 'em', 'm', 'a', 'e'

--- seeder.c
+++ seeder.c
@@ -4,10 +4,10 @@
 
 typedef struct {
   char *text;
-  char daytime[2];
+  int daytime;
 } Messages;
 
-void seeder(sqlite3 *db, char * your_name, char *err){
+void seeder(sqlite3 *db, char * your_name){
   time_t now;
   struct tm *clock;
   time(&now);
@@ -15,10 +15,10 @@ void seeder(sqlite3 *db, char * your_name, char *err){
   int namelen = strlen(your_name);
   clock = localtime(&now);
   Messages messages[] = {
-    {", my boy... why are you still awake? Hard programming night I see... I sense it..\n", EARLY_MORNING},
-    {"! Use your time wisely, you will. Trust in your judgement, I do\n", MORNING},
-    {", the chosen one you are\n", AFTERNOON},
-    {", remember this you will. Only the weak, the dark side of the force, embrace it\n", EVENING}
+    {", my boy... why are you still awake? Hard programming night I see... I sense it..", EARLY_MORNING},
+    {"! Use your time wisely, you will. Trust in your judgement, I do", MORNING},
+    {", the chosen one you are", AFTERNOON},
+    {", remember this you will. Only the weak, the dark side of the force, embrace it", EVENING}
   };
   buffer = (char *) malloc((namelen + strlen(messages[0].text) + 1) * 2);
   if(buffer == NULL){
@@ -26,7 +26,7 @@ void seeder(sqlite3 *db, char * your_name, char *err){
     return;
   }
   sprintf(buffer, "%s%s", your_name, messages[0].text);
-  insert_wise_message(buffer, messages[0].daytime, db, err);
+  insert_wise_message(buffer, messages[0].daytime, db);
   for (int i = 1; i < sizeof(messages) / sizeof(messages[1]); i++){
     if(strlen(messages[i].text) + namelen + 1 > strlen(buffer))
     {
@@ -38,7 +38,7 @@ void seeder(sqlite3 *db, char * your_name, char *err){
       }
     }
     sprintf(buffer, "%s%s", your_name, messages[i].text);
-    insert_wise_message(buffer, messages[i].daytime, db, err);
+    insert_wise_message(buffer, messages[i].daytime, db);
   }
   free(buffer);
 }
