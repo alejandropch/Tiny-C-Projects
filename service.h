5f41f620e514b8b5b7f594827ae7825956e3a3c9
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-15 22:57:07 -0500

    service.h: start using some macros so I start believing that I know how to program

--- service.h
+++ service.h
@@ -5,8 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sqlite3.h>
+
+#define DAYTIME_LIST \
+  X(EARLY_MORNING) \
+  X(MORNING) \
+  X(AFTERNOON) \
+  X(EVENING)
+
+enum Daytime {
+#define X(key) key,
+    DAYTIME_LIST
+#undef X
+  DAYTIME_LENGTH 
+};
   void print_random_message(sqlite3 *db, char *err);
-  void insert_wise_message(char *s, char *day_time, sqlite3 *db, char *err);
+  void insert_wise_message(char *s, int day_time, sqlite3 *db, char *err);
   bool exploded(int *rc, sqlite3 *db, char *err);
   char *allocate_message(char *s);
 #endif 
