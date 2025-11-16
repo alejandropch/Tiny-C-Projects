57d55007fa18613fe5ff84d6a223dec3cc3e2fa6
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-16 16:19:16 -0500

    main.c: using enums's keys for managing the daytimes, instead of 'em', 'm', 'a', 'e'

--- main.c
+++ main.c
@@ -6,17 +6,23 @@
 #include "seeder.h"
 #include "service.h"
 
-int get_valid_daytime(char *s){
-  int len = strlen(s);
-  if(len > 2){
-#define X(name) #name,
-    fprintf(stderr, "your daytime is too big bro. (%s)\n", DAYTIME_LIST);
+char *get_valid_day_time_text (){
+#define X(name) #name ","
+  char *str_daytimes = DAYTIME_LIST "\0";
 #undef X
-    return false;
-  }
-  char *valid_day_times[] =  { "em", "m", "a", "e"};
+  char *buffer = malloc(strlen(str_daytimes) + 1);
+  strcpy(buffer, str_daytimes);
+  buffer[strlen(buffer) - 1] = '\0'; // removing the trailing comma
+  return str_daytimes;
+}
+
+int get_valid_daytime(char *day_time){
+#define X(name) #name,
+  char *valid_day_times[] =  { DAYTIME_LIST };
+#undef X
+  char *str_daytimes = get_valid_day_time_text();
   for(int i = 0; i < DAYTIME_LENGTH; i++)
-    if(strcmp(valid_day_times[i], s) == 0)
+    if(strcmp(valid_day_times[i], day_time) == 0)
     {
       return i;
     }
@@ -29,14 +35,14 @@ int main(int argc, char *argv[]){
   sqlite3 *db;
   char *time, *message, *err;
   int rc = sqlite3_open("full_of_wisdom_this_db_is.db", &db);
-  if(exploded(&rc, db, err))
+  if(exploded(&rc, db))
   {
     fprintf(stderr, "error when opening a connection to SQLite3: %s\n", err);
     return 1;
   }
-  const char sql_create[] = "CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY_KEY, text VARCHAR(255), daytime VARCHAR(2));";
+  const char sql_create[] = "CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY_KEY, text VARCHAR(255), daytime INT);";
   rc = sqlite3_exec(db, sql_create, 0 , 0, &err);
-  if(exploded(&rc, db, err))
+  if(exploded(&rc, db))
   {
     fprintf(stderr, "error when creating table: %s\n", err);
     return 1;
@@ -49,7 +55,7 @@ int main(int argc, char *argv[]){
       if(strcmp(argv[i + 1], "-n") == 0 || strcmp(argv[i + 1], "--name") == 0)
       {
         char *name = argv[i + 2];
-        seeder(db, name, err);
+        seeder(db, name);
         return 0;
       }
     }
@@ -58,19 +64,21 @@ int main(int argc, char *argv[]){
         if(strcmp(argv[i + 1], "-t") == 0 || strcmp(argv[i + 1], "--time") == 0)
         {
           time = argv[i + 2];
-          if(is_valid_daytime(time) == false)
+          int daytime_code = get_valid_daytime(time);
+          char *str_daytimes = get_valid_day_time_text();
+          if(daytime_code == -1)
           {
-            fprintf(stderr, "bad option, you forgot the day time bro. (em, m, a, e)");
+            fprintf(stderr, "bad option, you forgot the day time bro. (%s)", str_daytimes);
             return 1;
           }
-          message = argv[i + 3]; 
-          insert_wise_message(message, time, db, err);
+          message = argv[i + 3];
+          insert_wise_message(message, daytime_code, db);
           return 0;
         }
       }
     }
   // no flag, just print out some saved message
-  print_random_message(db, err);
+  print_random_message(db);
 
   return 0; 
 }
