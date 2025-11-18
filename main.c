497366f2588de2ea167d6f775fde99e7cad194ca
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-17 21:01:32 -0500

    main: help flag

--- main.c
+++ main.c
@@ -49,6 +49,10 @@ int main(int argc, char *argv[]){
   }
   for (int i = 1; i < argc; i++)
   {
+    if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+    {
+
+    }
     if(strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--seed") == 0)
     {
 
